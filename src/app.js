8383df8758b11f75e6c07496dfcc7a50b9f4af04
eleezs <wilduche@gmail.com>
2022-10-30 11:13:58 +0100

    deploy to master

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
 const router = require('./routes')
+
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 
-app.get('/api/v1/', (req, res) => {
-    res.send(`Welcome to Uchenna Obidike HNG Project v.1.0 ${new Date()}`)
+app.get('/', (req, res) => {
+    res.status(200).send(`Welcome to Uchenna Obidike HNG Project v.1.0 ${new Date()}`)
 })
 
-app.use('/uchenna-obidike-hng-project/api/v1', router)
+app.use('/api/v1', router)
 
 // 404 error
 app.all('*', (req, res) => {
-	res.send(404, 'Endpoint Not Found');
+	res.status(400).send('Endpoint Not Found');
 });
 
 
