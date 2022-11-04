9579c85156842b8584d4e8ea0b2d63970a9b42bb
eleezs <wilduche@gmail.com>
2022-11-04 22:49:24 +0100

    task 2

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const task1  = require('./task1');
+const task2 = require('./task2')
 const router = express.Router();
 
 router.use('/task-1', task1);
+router.use('/task-2', task2);
 
 module.exports = router;
